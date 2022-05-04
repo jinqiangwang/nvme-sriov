bdc423994c2d7ad621e5a03b466314e9c9213a1d
Andy Wang <wangjinqiang@dapustor.com>
2022-05-04 21:57:46 +0800

    consolidate sr-iov vf / ns creationg / configuration

---
+++ cfg_vf_ns.sh
@@ -0,0 +1,73 @@
+#!/bin/bash 
+
+ns_cnt=
+export nvme_dev=
+example_str="example:\n\t $0 -d nvme0 -n 4\n"
+nvme_cmd=${nvme_cmd-./nvme}
+
+while getopts "n:d:" opt
+do
+    case $opt in 
+    n)
+        ns_cnt=$OPTARG
+        ;;
+    d)
+        export nvme_dev=$OPTARG
+        ;;
+    *)
+        echo -e ${example_str}
+        exit
+    esac
+done
+
+if [[ -z "${nvme_dev}" ]] || [[ `ls /dev/${nvme_dev} > /dev/null 2>&1; echo $?` -ne 0 ]]
+then
+    echo -e "incorrect nvme dev name, please check dev name. \n${example_str}"
+    exit
+fi
+
+source nvme-sriov.cfg 
+source nvme-func
+
+# check_nvme_cli ${nvme_cmd}
+# if [ $? -ne 0 ]; then exit 1; fi
+
+if [ -z ${hw_max_vf_cnt} ]
+then
+    echo "please check if sr-iov is supported on ${nvme_dev}"
+    exit
+fi
+
+echo "dev: ${nvme_dev}; capacity: ${drive_cap}; hw_max_vf_cnt: ${hw_max_vf_cnt}; max_vf_cnt: ${max_vf_cnt}"
+
+if [[ -z "${ns_cnt}" ]] || [[ $((${ns_cnt})) -le 0 ]] || [[ ${ns_cnt} -gt ${max_vf_cnt} ]]
+then
+    echo -e "incorrect ns count, ns need to be in [1, 32]. \n${example_str}"
+    exit
+fi
+
+# detach all NS
+detach_all_ns ${nvme_dev}
+
+# delete all NS
+delete_all_ns ${nvme_dev}
+
+# disable all VF
+switch_vf ${nvme_dev} 0
+
+# offline all VF in online status
+offline_all_vf ${nvme_dev}
+
+# create NS
+create_all_ns ${nvme_dev} ${ns_cnt}
+
+# assign resouse to VFs and bring them online
+config_all_vf ${nvme_dev} ${ns_cnt}
+
+# attach NS to VF
+attach_all_ns ${nvme_dev}
+
+# enable all VFs
+switch_vf ${nvme_dev} ${ns_cnt}
+
+watch -n 3 nvme list
---
+++ recfg_max_vf.sh
@@ -0,0 +1,68 @@
+#!/bin/bash 
+
+new_vf_cnt=
+nvme_dev=
+example_str="example:\n\t $0 -d nvme0 -n 4\n"
+nvme_cmd=${nvme_cmd-./nvme}
+
+while getopts "n:d:" opt
+do
+    case $opt in 
+    n)
+        new_vf_cnt=$OPTARG
+        ;;
+    d)
+        nvme_dev=$OPTARG
+        ;;
+    *)
+        echo -e ${example_str}
+        exit
+    esac
+done
+
+if [[ -z "${nvme_dev}" ]] || [[ `ls /dev/${nvme_dev} > /dev/null 2>&1; echo $?` -ne 0 ]]
+then
+    echo -e "incorrect nvme dev name, please check dev name. \n${example_str}"
+    exit
+fi
+
+source nvme-sriov.cfg
+source nvme-func
+
+echo "dev: ${nvme_dev}; drive_cap_gb: ${drive_cap_gb}; hw_max_vf_cnt: ${hw_max_vf_cnt}; max_vf_cnt: ${max_vf_cnt}"
+
+if [ -z ${hw_max_vf_cnt} ]
+then
+    echo "please check if sr-iov is supported on ${nvme_dev}"
+    exit
+fi
+
+if [[ -z "${new_vf_cnt}" ]] || [[ $((${new_vf_cnt})) -le 0 ]] || [[ ${new_vf_cnt} -gt ${hw_max_vf_cnt} ]]
+then
+    echo -e "incorrect ns count, ns need to be in [1, ${hw_max_vf_cnt}]. \n${example_str}"
+    exit
+fi
+
+detach_all_ns ${nvme_dev}
+delete_all_ns ${nvme_dev}
+offline_all_vf ${nvme_dev}
+
+# disable all VF
+switch_vf ${nvme_dev} 0
+
+${nvme_vu} dapu set-sriov /dev/${nvme_dev} -n ${new_vf_cnt}
+
+# try power down / up
+# slot=$(grep `cat /sys/class/nvme/${nvme_dev}/address | cut -d\. -f 1` /sys/bus/pci/slots/*/address | cut -d/ -f 1-6)
+# echo 0 > ${slot}/power
+# sleep 5
+# echo 1 > ${slot}/power
+
+# try to remove / rescan the pcie device
+pcie_dev=$(find /sys/devices/pci* -name `cat /sys/class/nvme/${nvme_dev}/address` | grep -v iommu)
+parent_dev=${pcie_dev%/*}
+echo 1 > ${pcie_dev}/remove
+echo 1 > ${parent_dev}/rescan
+
+sleep 5
+${nvme_cmd} list-secondary /dev/${nvme_dev}
